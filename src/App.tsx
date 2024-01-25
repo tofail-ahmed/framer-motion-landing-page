430ec825285c2cef1517c7db10f3187ebdbff1a4
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-25 10:42:24 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className='text-center text-3xl font-semibold text-emerald-700'>React + Tailwind + React Router Dom</h1>
+    </div>
+  )
+}
+
+export default App
