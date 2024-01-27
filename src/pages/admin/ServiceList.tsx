80cdd79d1a046f8e81766257d4b88ef50dffe79e
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-27 15:16:39 +0600

    server connected

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ServiceList = () => {
+  const [sevices,setServices]=useState([]);
+  useEffect(()=>{
+    fetch(`http://localhost:5000/api/v1/services`)
+    .then(res=>res.json())
+    .then(data=>setServices(data.data))
+  },[])
+  console.log(sevices)
   return (
     <div>ServiceList</div>
   )
